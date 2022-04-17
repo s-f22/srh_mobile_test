f0097ed35fdf3e0c44064d1e0e2ce1e3d2c904de
Samuel <samuel.figs@gmail.com>
2022-04-16 21:55:31 -0300

    login

--- src/pages/signin/SignIn.js
+++ src/pages/signin/SignIn.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import * as Animatable from 'react-native-animatable'
 
 import {
   Text,
   View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 
@@ -10,10 +14,86 @@ import {
 export default function SignIn() {
 
   return (
-    <View>
-      <Text>Tela SignIn</Text>
+    <View style={styles.container}>
+      <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
+        <Text style={styles.message}>Bem-vindo(a)</Text>
+      </Animatable.View>
+
+      <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+        <Text style={styles.title}>CPF</Text>
+        <TextInput
+          placeholder='Digite seu CPF'
+          style={styles.input}
+        />
+        <Text style={styles.title}>Senha</Text>
+        <TextInput
+          placeholder='Digite sua senha'
+          style={styles.input}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonRegister}>
+          <Text style={styles.registerText}>Não tem conta? Solicite o cadastro ao seu gerente.</Text>
+        </TouchableOpacity>
+      </Animatable.View>
     </View>
   );
 };
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
+  containerHeader:{
+    marginTop: '14%',
+    marginBottom: '8%',
+    paddingStart: '5%',
+  },
+  message: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  containerForm: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: '5%',
+    paddingEnd: '5%'
+  },
+  title: {
+    fontSize: 20,
+    marginTop: 28,
+    fontWeight: 'bold'
+  },
+  input: {
+    borderBottomWidth: 1,
+    height: 40,
+    marginBottom: 12,
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: 'red',
+    width: '100%',
+    borderRadius: 4,
+    paddingVertical: 8,
+    marginTop: 14,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  buttonRegister: {
+    marginTop: 14,
+    alignSelf: 'center'
+  },
+  registerText: {
+    color: '#a1a1a1'
+  }
+})
--- src/pages/welcome/Welcome.js
+++ src/pages/welcome/Welcome.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import * as Animatable from 'react-native-animatable'
 import {
   Image,
   View,
@@ -8,26 +8,33 @@ import {
   Text
 } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native'
 
 export default function Welcome() {
+
+  const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
       <View style={styles.containerLogo}>
-        <Image
+        <Animatable.Image
+          animation="flipInY"
           source={require("../../../assets/imgWeb/logo.png")}
           style={{ width: '100%' }}
           resizeMode="contain"
         />
       </View>
 
-      <View style={styles.containerForm}>
+      <Animatable.View delay={600} animation="fadeInUp" style={styles.containerForm}>
         <Text style={styles.title}>Motivação e benefícios, tudo num só App!</Text>
         <Text style={styles.text}>Faça seu login para começar</Text>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('SignIn')}
+        >
           <Text style={styles.buttonText}>Acessar</Text>
         </TouchableOpacity>
-      </View>
+      </Animatable.View>
 
     </View>
   );
@@ -57,10 +64,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
     marginTop: 28,
-    marginBottom: 12
+    marginBottom: 12,
+    color: 'white'
   },
   text: {
-    color: '#a1a1a1',
+    color: 'white',
   },
   button: {
     position: 'absolute',
